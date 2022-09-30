7190b30dcf5d768a712c6b638e854d2777638d41
Noah Goss <95068655+RainbowCitrine@users.noreply.github.com>
2022-09-30 16:10:02 -0700

    Create Triggers.cs

---
+++ C# Scripts/Triggers.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+    //Add ur rigid bodys and collisions do not forget to set gravity to zero for certain games
+    void OnCollisionEnter2D(Collision2D other) 
+    {
+        Debug.Log("Oof"); 
+    }
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        Debug.Log("Hello?");
+    }
+}
